7858a3053f27a1265a1e315052b567b17e176bee
jjestepa <jesus.j.estepa@gmail.com>
2015-06-30 20:51:02 +0200

    Version 1
    primer commit

---
+++ random.js
@@ -0,0 +1,10 @@
+//Math.random() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
+
+if (numero <= 0.5)
+{
+    console.log("menor que 0,5");
+}
+else {
+    console.log("mayor que 0,5");
+}
